309549efc476e389ee7785e1e386ab38eace4b61
Dmitry Chepurovskiy <dm3chip@gmail.com>
2017-05-08 21:39:54 +0300

    Alpine based v2.1-alpha

--- startup.sh
+++ startup.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
-echo "Starting NFS Server"
+#!/bin/sh -eu
 
-rpcbind
-rpc.statd
-rpc.nfsd
+# echo "Starting NFS Server"
 
-echo "Started"
+# rpcbind
+# rpc.statd
+# rpc.nfsd
 
-exec rpc.mountd --foreground
-# echo "Done all tasks - Running continious loop to keep this container alive"
-# while true; do
-#   sleep 3600
-# done
+# echo "Started"
+
+# exec rpc.mountd --foreground
+# # echo "Done all tasks - Running continious loop to keep this container alive"
+# # while true; do
+# #   sleep 3600
+# # done
+/usr/sbin/exportfs -r
+/sbin/rpcbind --
+/usr/sbin/rpc.statd
+/usr/sbin/rpc.nfsd |:
+/usr/sbin/rpc.mountd -F
